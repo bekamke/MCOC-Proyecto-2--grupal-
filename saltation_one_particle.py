affda520c3ed395f7b2c9e70ccbc8e2b86d9c94e
smorenovial <53713496+smorenovial@users.noreply.github.com>
2019-10-01 16:41:17 -0300

    Update saltation_one_particle.py

--- saltation_one_particle.py
+++ saltation_one_particle.py
@@ -7,17 +7,17 @@ _s = 1.
 _mm = 1e-3*_m
 _gr = 1e-3*_kg 
 
+# velocidades iniciales
+vfx = 5.0*_m/_s
+vfy = 0.0*_m/_s
 
-vf_x = 5.0*_m/_s
-vf_y = 0.0*_m/_s
+x0 = array([0., 1.], dtype=double)
+v0 = array([1., 1.], dtype=double)
 
-x_0 = array([0., 1.], dtype=double)
-v_0 = array([1., 1.], dtype=double)
-
-x_i = zeros(2, dtype=double)      # posicion actual
-v_i = zeros(2, dtype=double)      # velocidad actual
-x_i_m1 = zeros(2, dtype=double)   # posicion siguiente
-v_i_m1 = zeros(2, dtype=double)   # velocidad siguiente
+xi = x0 #zeros(2, dtype=double)      # posicion actual
+vi = v0 #zeros(2, dtype=double)      # velocidad actual
+xim1 = zeros(2, dtype=double)   # posicion siguiente
+vim1 = zeros(2, dtype=double)   # velocidad siguiente
 
 g = 9.81*_m/(_s**2)         # gravedad
 d = 1*_mm                   # diametro de la particula
@@ -26,34 +26,30 @@ Cd = 0.47                   # coeficiente de Drag para particula esferica
 
 m = rho*(4./3./8.)*pi*(d**3) # masa de la particula
 
-
 # Inicializar Euler en x_0
 dt = 2e-6*_s    # paso de tiempo 
-tmax = 1*_s    # tiempo máximo de simulacion
+tmax = 0.1*_s    # tiempo maximo de simulacion
 ti = 0.*_s     # tiempo actual
 
 W = array([0., -m*g])  #Fuerza de peso 
-vf = array([vf_x, vf_y])
+vf = array([vfx, vfy])
 
-Nt = int32(2*tmax / dt)
-x_store = x_0 #zeros((2, Nt))
-v_store = v_0 #zeros((2, Nt))
+Nt = int32(2*tmax / dt) # numero de tiempos que tengo
+x_store = zeros((2, Nt))
+v_store = zeros((2, Nt))
 t_store = zeros(Nt)
 
-#metodo de euler
-x_store=[]
-v_store=[]
+# metodo de euler
 i = 0
-
 while ti < tmax:
 
 	if i % 100 == 0:
-		print "ti =", ti," |x| =", sqrt(dot(x_i,x_i))
+		print "ti =", ti," |xi| =", sqrt(dot(xi,xi))
     	#print "xi =", xi
     	#print "vi =", vi
 
 	# evaluar v relativa
-	vrel = vf - v_i                      # velocidad relativa
+	vrel = vf - vi                      # velocidad relativa
 	norm_vrel = sqrt(dot(vrel, vrel))   # norma velocidad relativa
 
 	# evaluar fuerzas sobre la particula
@@ -61,31 +57,33 @@ while ti < tmax:
 	Fi = W + fD                               # Sumatoria de todas las fuerzas que se ejercen sobre la particula
 
 	#print "Fi =", Fi
+
 	# evaluar aceleracion
 	ai = Fi / m
-
 	#print "a_i =", a_i
+
 	# integrar
-	xim1 = x_i + v_i*dt + ai*(dt**2/2)
-	vim1 = v_i + ai*dt
+	xim1 = xi + vi*dt + ai*(dt**2/2) # base del algoritmo, se calcula la posicion con esta formula
+	vim1 = vi + ai*dt
 
 	# avanzar al siguiente paso
+	x_store[:, i] = xi
+	v_store[:, i] = vi
 	t_store[:] = ti
 
 	ti += dt
 	i += 1
-	x_i = xim1
-	v_i = vim1
-	x_store.append(x_i)
-	v_store.append(v_i)
+	xi = xim1
+	vi = vim1
+	
 
 # guardar ultimo paso
-x_store.append(x_i)
-v_store.append(v_i)
-#t_store[:] = ti
+x_store[:, i] = xi
+v_store[:, i] = vi
+t_store[i] = ti
 
-#print x_store
-#print v_store
+print x_store
+
 figure()
-plot(x_store, v_store)
+plot(x_store[0, :i], x_store[1, :i])
 show()
