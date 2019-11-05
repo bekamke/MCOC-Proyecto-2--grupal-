2772eb80890ed0670044cfa183c98c71f3df9100
smorenovial <53713496+smorenovial@users.noreply.github.com>
2019-11-05 18:48:23 -0300

    Update entrega 6.py

--- entrega 6.py
+++ entrega 6.py
@@ -2,53 +2,52 @@
 """
 crear archivo de texto o carpeta con datos de partículas en cada instante
 """
-
 from matplotlib.pylab import *
 from parameters import *
-from funciones import *
-import time 
+from functions import *
+import time
+import scipy as sp
 
-reuse_initial_condition = True
-#reuse_initial_condition = False
+norm = lambda v: sqrt(dot(v,v))
+
+#reuse_initial_condition = True
+reuse_initial_condition = False
 
 doit = True
 #doit = False
 
-start = time.time()
-tiempo_bloque_1 = 0
-tiempo_bloque_2 = 0
+
 t = arange(0, tmax, dt)
 Nt = len(t)
 
-Nparticulas = 4
-tmax = 0.005
+Nparticulas = 20
 
 
 if reuse_initial_condition:
-    print "Reusing initial conditions"
+    print "reusing initial conditions"
     data = load("initial_condition.npz")
-    x0 = data["x0"]    #posición en x
-    y0 = data["y0"]    #posición en y
+    x0 = data["x0"]    #posicion en x
+    y0 = data["y0"]    #posicion en y
     vx0 = data["vx0"]  #velocidad en x
     vy0 = data["vy0"]  #velocidad en y
     Nparticulas = data["Nparticulas"]  #cantidad de particulas con que se trabaja
 else:
-    print "Generating new initial conditions"
+    print "generating new initial conditions"
     itry = 1
     while True:
          dmin = infty
          x0 = 800 * d * rand(Nparticulas)
-         y0 = 5* d * rand(Nparticulas) + i*d
+         y0 = 5* d * rand(Nparticulas) + 1*d
          for i in range(Nparticulas):
              xi, yi = x0[i], y0[i]
              for j in range(i+1, Nparticulas):
                  xj, yj = x0[j], y0[j]
                  dij = sqrt((xi - xj)**2 + (yi - yj)**2)
                  dmin = min(dmin, dij)
-         print "Try # ", intry, "dmin/d = ", dmin/d
+         print "Try # ", itry, "dmin/d = ", dmin/d
          if dmin > 0.9 * d:
              break
-         intry += 1
+         itry += 1
          
     vx0 = ustar * rand(Nparticulas)
     vy0 = 0
@@ -61,8 +60,8 @@ Nt = len(t)
 from scipy.integrate import odeint
 
 #guardar solo pasos necesarios 
-zk = zeros((4*Nparticulas)) #guardar paso zk
-zkm1 = zeros((4*Nparticulas)) #guardar paso zkm1
+zk = sp.zeros((4*Nparticulas)) #guardar paso zk
+zkm1 = sp.zeros((4*Nparticulas)) #guardar paso zkm1
 
 #condiciones iniciales para zk en tiempo = 0
 zk[0::4] = x0
@@ -89,14 +88,14 @@ fout_parametros["Cm"] = Cm
 fout_parametros["CL"] = CL
 fout_parametros["Rp"] = Rp
 fout_parametros["ustar"] = ustar
-fout_parametros["au_star"] = au_star
+fout_parametros["tau_star"] = tau_star
 fout_parametros["R"] = R
 fout_parametros["alpha"] = alpha
 fout_parametros["ihat"] = ihat
 fout_parametros["jhat"] = jhat
 fout_parametros["tau_cr"] = tau_cr
 fout_parametros["A"] = A
-fout_parametros["k_penal"] = K_penal
+fout_parametros["k_penal"] = k_penal
 
 fout_z = fout.create_dataset("z", (Nt, 1 + 4 * Nparticulas), dtype = double)
 # print Nt
@@ -112,8 +111,8 @@ import time
 
 start = time.time()
 
-timepo_bloque_1 = 0
-timwpo_bloque_2 = 0
+tiempo_bloque_1 = 0
+tiempo_bloque_2 = 0
 
 if doit:
     while dt*k < int(tmax/dt - 1)*dt:
@@ -121,19 +120,19 @@ if doit:
         # fout.write("{}", format(dt*k))
         ti = time.time()
        
-        #en cada paso de tiempo se guarda el estado de cada partícula
+        #en cada paso de tiempo se guarda el estado de cada particula
         # savetxt(fout, zk, fmt = "%.10e ", newline = "")
        
         fout_z[k, 0] = dt * k
         fout_z[k, 1:] = zk
         
         tf = time.time()
-        timepo_bloque_1 += tf -ti
+        tiempo_bloque_1 += tf -ti
         
         #fout.write("\n")
         
         if k % 100 == 0:
-            print "k = {}   t = {}  ", format(k, k*dt)
+            print "k = {}   t = {}  ".format(k, k*dt)
         done *= 0
         
         ti= time.time()
@@ -156,7 +155,7 @@ if doit:
                     
                     rij = zk_j[0:2] - zk_i[0:2]
 
-                    if norm(rij) < 0.5*(di+dj)*3:
+                    if norm(rij) < 0.5*(d-dj)*3: #ver si es mas o menos
                         hay_impacto = True
                         impacting_set[0] = i
                         impacting_set[M] = j
@@ -169,7 +168,7 @@ if doit:
                         zk_j = zk[jrange]
                         zk_all = hstack((zk_all, zk_j))
                     
-                    zkm1_all = odeint(zp_M_particulas, zk_all, [dt*k, dt*(k+1)], args = (M, ))
+                    zkm1_all =odeint(zp_M_particulas, zk_all, [dt*k, dt*(k+1)], args = (M,))
                     
                     zkm1[irange] = zkm1_all[1,0:4]
                     
@@ -188,7 +187,7 @@ if doit:
                     done[i] = 1
         
         tf = time.time()
-        timepo_bloque_2 = tf - ti
+        tiempo_bloque_2 = tf - ti
         
         zk = zkm1
         k += 1 #k representa paso de tiempo
@@ -197,11 +196,9 @@ end = time.time()
 
 
 print "tiempo_bloque_1: ", tiempo_bloque_1
-print "timepo_bloque_2: ", tiempo_bloque_2
+print "tiempo_bloque_2: ", tiempo_bloque_2
 print "tiempo total: ", end - start
 
-print "Tiempo de escritura es {}\% del tiempo total.", format(tiwmpo_bloque_1/(end - start) * 100)
+print "Tiempo de escritura es {}\% del tiempo total.", format(tiempo_bloque_1/(end - start) * 100)
 
-fout.close()
-
-                     
+fout.close()                    
